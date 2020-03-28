87cc3076cdfa11468e14e94100c1611778b9053d
lovaszbotond <botond.lovasz@gmail.com>
2020-03-28 17:34:15 +0100

    Add Logger.error - warn - info - debug - trace to the main class

--- src/main/java/pkg/main.java
+++ src/main/java/pkg/main.java
@@ -1,4 +1,15 @@
 package pkg;
 
+import org.tinylog.Logger;
+
 public class main {
+
+    public static void main(String[] args) {
+        Logger.error("THis is an ERROR message");
+        Logger.warn("This is a WARN message");
+        Logger.info("This is an INFO message");
+        Logger.debug("This is a DEBUG message");
+        Logger.trace("This is a TRACE message");
+        
+    }
 }
